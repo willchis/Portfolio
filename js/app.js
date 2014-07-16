ca755bef402b7885ed73834f006ce1c1b6d850c4
Will <william.chislett@durabledigital.com>
2014-07-16 23:10:11 +0100

    Updating transitions and adding angular animations

--- js/app.js
+++ js/app.js
@@ -1,15 +1,21 @@
-var portfolioApp = angular.module('portfolioApp', ['ngRoute']);
+var portfolioApp = angular.module('portfolioApp', ['ngRoute', 'ngAnimate']);
 
 portfolioApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
-      when('/CV', {
+      when('/cv', {
         templateUrl: 'partials/cv.html',
         controller: 'HomeController'
       }).
-      otherwise({
-        redirectTo: '/',
+      when('/experiments', {
+        templateUrl: 'partials/experiments.html',
+        controller: 'HomeController'
+      }).
+      when('/home', {
         templateUrl: 'partials/home.html',
         controller: 'HomeController'
+      }).
+      otherwise({
+        redirectTo: '/home'
       });
   }]);
